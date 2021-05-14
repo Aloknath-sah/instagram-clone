d4bbf97e8b283d5ae436700043fa2bfe5af2611d
Aloknath-sah <kumaraditya6396@gmail.com>
2021-05-14 14:21:14 +0530

    basic structure of files created

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="navbar_first">
+                <div className="navbar_first-logo">
+                    <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="insta_logo" />
+                </div>
+            </div>
+            <div className="navbar_middle"></div>
+            <div className="navbar_last"></div>
+        
+        </div>
+    );
+}
+
+export default Navbar;
---
+++ frontend/src/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBrbAH4AtQZ-Uw6Oyik8TiH9t_QJ_-K1lM",
+    authDomain: "instagram-79dbf.firebaseapp.com",
+    projectId: "instagram-79dbf",
+    storageBucket: "instagram-79dbf.appspot.com",
+    messagingSenderId: "410016957907",
+    appId: "1:410016957907:web:5a7f39623cb79b6d395b26",
+    measurementId: "G-N33KLV4SJF"
+  };
